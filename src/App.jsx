d25187392ee5f481a264d13b4db4419ba8226745
MerMer69 <104286924+SheLuvsAlex@users.noreply.github.com>
2022-08-26 21:57:49 +0000

    landing section updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Test from './components/test';
+import Landing from './components/landing';
+
 
 function App() {
   return (
     <>
-    <Test name={'mrs. roy'} color = {'green'}/>
-    <Test name={'mrs. alrx'} color = {'yellow'}/>
-      <Test name={'max'} color = {'pink'}/>
+  <Landing/>
+      
      
     </>
   );
 }
 
-export default App;
+export default App
+    //   <Test name={'mrs. roy'} color = {'green'}/>
+    // <Test name={'mrs. alrx'} color = {'yellow'}/>
+    // <Test name={'max'} color = {'pink'}/>
---
+++ src/components/landing.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import React from 'react'
+import {VscChevronLeft,VscChevronRight} from 'react-icons/vsc';
+
+function Landing() {
+
+  return(
+
+     <Container>
+     <Wraper>
+      <ImageBG src={'Flag.jpg'}/>
+
+
+       <ArrowGroup>
+         <ArrowBox><VscChevronLeft /></ArrowBox>
+         <ArrowBox><VscChevronRight /></ArrowBox>
+       </ArrowGroup>
+     
+     
+     </Wraper>
+     </Container>
+  );
+
+}
+
+export default Landing;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: red;
+  
+`
+const Wraper = styled.div`
+   height: 100%;
+  width: 100%;
+`
+const ImageBG = styled.img`
+ 
+  max-width: 100%;
+  height: 100%;
+
+`
+const ArrowGroup = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transform: translate(0,-50%);
+  display: flex;
+  justify-content: space-between;
+/*   background-color: yellow; */
+`
+const ArrowBox = styled.div`
+  margin: 0 1rem;
+  color: white;
+   background-color: rgba(250,250,250,.5); 
+/*    opacity: 0.5; */
+padding: 1.5%;
+&:hover{
+  cursor: pointer;
+}
+`
